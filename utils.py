c5ff38fe3ccc075910579418e9c3b66d94ff9301
frankgeorge <frankgeorge033@gmail.com>
2021-02-28 07:44:26 +0530

    added key

--- utils.py
+++ utils.py
@@ -1,9 +1,9 @@
 import os
-os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "jokes-ubfo-63bb41545fc8.json"
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "online-shopping-eknm-30aa5c5469c2.json"
 
 import dialogflow_v2 as dialogflow
 dialogflow_session_client = dialogflow.SessionsClient()
-PROJECT_ID = "jokes-ubfo"
+PROJECT_ID = "online-shopping-eknm"
 
 def detect_intent_from_text(text, session_id, language_code='en'):
     session = dialogflow_session_client.session_path(PROJECT_ID, session_id)
